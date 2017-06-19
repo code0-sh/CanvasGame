00d0d3d90685ab498f6fb7507f619651fe038426
omura.522 <omura.522@fssoftware.co.jp>
2017-06-19 10:37:35 +0900

    ピッチのサイズを変更する

--- src/Pitch.js
+++ src/Pitch.js
@@ -1,9 +1,9 @@
 import { canvas, ctx } from './DOM';
-import { PITCH_COLOR, PITCH_LINE_COLOR } from './Constant';
+import { PITCH_MARGIN, PITCH_COLOR, PITCH_LINE_COLOR } from './Constant';
 
 export default class Pitch {
   draw() {
-    // Outer lines
+    // Ground lines
     ctx.beginPath();
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = PITCH_COLOR;
@@ -15,16 +15,33 @@ export default class Pitch {
 
     ctx.fillStyle = PITCH_LINE_COLOR;
 
+    // Outer lines
+    ctx.beginPath();
+    ctx.rect(
+      PITCH_MARGIN,
+      PITCH_MARGIN,
+      canvas.width - PITCH_MARGIN * 2,
+      canvas.height - PITCH_MARGIN * 2
+    );
+    ctx.fillStyle = PITCH_COLOR;
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = PITCH_LINE_COLOR;
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.fillStyle = PITCH_LINE_COLOR;
+
     // Mid line
     ctx.beginPath();
-    ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.moveTo(PITCH_MARGIN, canvas.height / 2);
+    ctx.lineTo(canvas.width - PITCH_MARGIN, canvas.height / 2);
     ctx.stroke();
     ctx.closePath();
 
     //Mid circle
     ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 73, 0, 2 * Math.PI, false);
+    ctx.arc(canvas.width / 2, canvas.height / 2, 25, 0, 2 * Math.PI, false);
     ctx.stroke();
     ctx.closePath();
     //Mid point
@@ -35,18 +52,28 @@ export default class Pitch {
 
     //Home penalty box
     ctx.beginPath();
-    ctx.rect((canvas.width - 322) / 2, 0, 322, 132);
+    ctx.rect((canvas.width - 150) / 2, PITCH_MARGIN, 150, 50);
     ctx.stroke();
     ctx.closePath();
     //Home goal box
     ctx.beginPath();
-    ctx.rect((canvas.width - 146) / 2, 0, 146, 44);
+    ctx.rect((canvas.width - 100) / 2, PITCH_MARGIN, 100, 20);
     ctx.stroke();
     ctx.closePath();
     //Home goal
     ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - 22, 1);
-    ctx.lineTo(canvas.width / 2 + 22, 1);
+    ctx.moveTo(
+      canvas.width / 2 - 22,
+      PITCH_MARGIN + 2,
+      canvas.width / 2 - 22,
+      1
+    );
+    ctx.lineTo(
+      canvas.width / 2 + 22,
+      PITCH_MARGIN + 2,
+      canvas.width / 2 + 22,
+      1
+    );
     ctx.lineWidth = 2;
     ctx.stroke();
     ctx.closePath();
@@ -54,44 +81,78 @@ export default class Pitch {
 
     //Home penalty point
     ctx.beginPath();
-    ctx.arc(canvas.width / 2, 88, 1, 0, 2 * Math.PI, true);
+    ctx.arc(canvas.width / 2, PITCH_MARGIN + 35, 1, 0, 2 * Math.PI, true);
     ctx.fill();
     ctx.closePath();
     //Home half circle
     ctx.beginPath();
-    ctx.arc(canvas.width / 2, 88, 73, 0.79 * Math.PI, 0.21 * Math.PI, true);
+    ctx.arc(
+      canvas.width / 2,
+      PITCH_MARGIN + 35,
+      25,
+      0.79 * Math.PI,
+      0.21 * Math.PI,
+      true
+    );
     ctx.stroke();
     ctx.closePath();
 
     //Away penalty box
     ctx.beginPath();
-    ctx.rect((canvas.width - 322) / 2, canvas.height - 132, 322, 132);
+    ctx.rect(
+      (canvas.width - 150) / 2,
+      canvas.height - 50 - PITCH_MARGIN,
+      150,
+      50
+    );
     ctx.stroke();
     ctx.closePath();
     //Away goal box
     ctx.beginPath();
-    ctx.rect((canvas.width - 146) / 2, canvas.height - 44, 146, 44);
+    ctx.rect(
+      (canvas.width - 100) / 2,
+      canvas.height - 20 - PITCH_MARGIN,
+      100,
+      20
+    );
     ctx.stroke();
     ctx.closePath();
     //Away goal
     ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - 22, canvas.height - 1);
-    ctx.lineTo(canvas.width / 2 + 22, canvas.height - 1);
+    ctx.moveTo(
+      canvas.width / 2 - 22,
+      canvas.height - PITCH_MARGIN - 2,
+      canvas.width / 2 - 22,
+      canvas.height - 1
+    );
+    ctx.lineTo(
+      canvas.width / 2 + 22,
+      canvas.height - PITCH_MARGIN - 2,
+      canvas.width / 2 + 22,
+      canvas.height - 1
+    );
     ctx.lineWidth = 2;
     ctx.stroke();
     ctx.closePath();
     ctx.lineWidth = 1;
     //Away penalty point
     ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height - 88, 1, 0, 2 * Math.PI, true);
+    ctx.arc(
+      canvas.width / 2,
+      canvas.height - PITCH_MARGIN - 35,
+      1,
+      0,
+      2 * Math.PI,
+      true
+    );
     ctx.fill();
     ctx.closePath();
     //Away half circle
     ctx.beginPath();
     ctx.arc(
       canvas.width / 2,
-      canvas.height - 88,
-      73,
+      canvas.height - PITCH_MARGIN - 35,
+      25,
       1.21 * Math.PI,
       1.79 * Math.PI,
       false
@@ -101,22 +162,43 @@ export default class Pitch {
 
     //Home R corner
     ctx.beginPath();
-    ctx.arc(0, 0, 8, 0, 0.5 * Math.PI, false);
+    ctx.arc(PITCH_MARGIN, PITCH_MARGIN, 8, 0, 0.5 * Math.PI, false);
     ctx.stroke();
     ctx.closePath();
     //Home L corner
     ctx.beginPath();
-    ctx.arc(canvas.width, 0, 8, 0.5 * Math.PI, 1 * Math.PI, false);
+    ctx.arc(
+      canvas.width - PITCH_MARGIN,
+      PITCH_MARGIN,
+      8,
+      0.5 * Math.PI,
+      1 * Math.PI,
+      false
+    );
     ctx.stroke();
     ctx.closePath();
     //Away L corner
     ctx.beginPath();
-    ctx.arc(0, canvas.height, 8, 0, 2 * Math.PI, true);
+    ctx.arc(
+      PITCH_MARGIN,
+      canvas.height - PITCH_MARGIN,
+      8,
+      2 * Math.PI,
+      1.5 * Math.PI,
+      true
+    );
     ctx.stroke();
     ctx.closePath();
     //Away R corner
     ctx.beginPath();
-    ctx.arc(canvas.width, canvas.height, 8, 1 * Math.PI, 1.5 * Math.PI, false);
+    ctx.arc(
+      canvas.width - PITCH_MARGIN,
+      canvas.height - PITCH_MARGIN,
+      8,
+      1 * Math.PI,
+      1.5 * Math.PI,
+      false
+    );
     ctx.stroke();
     ctx.closePath();
   }
